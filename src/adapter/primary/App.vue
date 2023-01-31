424ccf50d328b4632486defd8f11d9021a60a50e
Foetus6907 <mghoubali@gmail.com>
2023-01-31 08:54:32 +0100

    Set up AppLayout

--- src/adapter/primary/App.vue
+++ src/adapter/primary/App.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from "./views/HomeView.vue";
+import logo from "@/adapter/primary/assets/img/img.png";
 </script>
 
 <template>
-  <HelloWorld />
+  <q-layout view="hHr lpr fFr" class="overflow-hidden-y">
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <img :src="logo" alt="logo" style="width: 256px" />
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="overflow-hidden-y">
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<style lang="scss"></style>
+<style lang="sass" scoped>
+#logo
+  width: 15%
+
+.q-header
+  height: 5.5vh
+.q-page-container
+  height: calc(100% - 5.5vh)
+</style>
--- src/adapter/primary/router/router.ts
+++ src/adapter/primary/router/router.ts
@@ -1,8 +1,12 @@
 import HomeView from "@/adapter/primary/views/HomeView.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
-import AppLayout from "@/adapter/primary/Layout.vue";
 
-const routes = [{ path: "/", component: AppLayout, children: [{ path: "", component: HomeView }] }];
+const routes = [
+  {
+    path: "/",
+    component: HomeView,
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
